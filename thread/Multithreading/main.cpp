f414b2bcc9bd675c0ca9c85f4e8051989603c14a
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2022-10-13 19:23:50 +0300

    Update main.cpp

--- thread/Multithreading/main.cpp
+++ thread/Multithreading/main.cpp
@@ -9,13 +9,13 @@ int main() {
   const int size = 3;
   Matrix a(size, size);
   a.fillMatrix();
-  a.print();
+  //a.print();
   Matrix b(size, size);
   b.fillMatrix();
-  b.print();
+  //b.print();
   auto start = std::chrono::steady_clock::now();
   Matrix result = a * b;
-  result.print();
+  //result.print();
   auto end = std::chrono::steady_clock::now();
   int time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
   std::cout << "1 thread calculation" << std::setw(5) << " time in microseconds: " << time << std::endl;
@@ -23,7 +23,7 @@ int main() {
   for (int blockSize = 1; blockSize <= size; blockSize++) {
     auto start = std::chrono::steady_clock::now();
     Matrix result = BlockMatrix::multiply(a, b, blockSize);
-    result.print();
+    //result.print();
     auto end = std::chrono::steady_clock::now();
     int time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
     std::cout << "size of block: " << blockSize << std::setw(5) << " time in microseconds: " << time << std::endl;
